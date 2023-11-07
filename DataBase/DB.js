6a0d95ebc1209836a95352b7bf4d389b01dd618b
Somashekar N <n.somashekar0007@gmail.com>
2023-11-07 21:19:11 +0530

    second  commit

--- DataBase/DB.js
+++
@@ -1,3 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb://127.0.0.1:27017/CRMDatabase").then(()=>console.log("CRM Database Connected")).catch(()=>console.log("Something wrong"))
--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3001
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
-require("./DataBase/DB")
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(express.json())
+// Middleware to parse JSON requests
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello Buddy')
-})
+// Create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'your-email@gmail.com',
+    pass: 'your-password'
+  }
+});
+
+// Define an endpoint for sending emails
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to,
+    subject,
+    text
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error sending email' });
+    } else {
+      console.log('Email sent: ' + info.response);
+      res.json({ message: 'Email sent successfully' });
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
